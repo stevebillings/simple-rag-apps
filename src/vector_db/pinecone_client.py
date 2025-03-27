5f7592a61f73d5d242da32e5ca6becd19455a833
Steve Billings <billings.steve@gmail.com>
2025-03-27 14:05:41 -0400

    upsert in chunks to avoid hitting max upsert request size

--- src/vector_db/pinecone_client.py
+++ src/vector_db/pinecone_client.py
@@ -11,18 +11,27 @@ class PineconeClient:
         pinecone_index_name: str,
         pinecone_namespace: str,
         query_response_parser: PineconeQueryResponseParser,
+        batch_size: int = 100,
     ) -> None:
         self._pinecone_namespace = pinecone_namespace
         self._query_response_parser = query_response_parser
+        self._batch_size = batch_size
         pinecone_api_key = os.getenv("PINECONE_API_KEY")
         pc = Pinecone(api_key=pinecone_api_key)
         self._index: Index = pc.Index(pinecone_index_name)
 
     def upsert(self, vectors: List[Dict[str, Any]]) -> None:
-        self._index.upsert(
-            vectors=vectors,
-            namespace=self._pinecone_namespace,
-        )
+        try:
+            # Process vectors in batches
+            for i in range(0, len(vectors), self._batch_size):
+                batch = vectors[i : i + self._batch_size]
+                self._index.upsert(
+                    vectors=batch,
+                    namespace=self._pinecone_namespace,
+                )
+        except Exception as e:
+            print(e)
+            raise e
 
     def query(self, query_embedding: List[float], top_k: int = 1) -> List[str]:
         response = self._index.query(
