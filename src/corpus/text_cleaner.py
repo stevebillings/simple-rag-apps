eff5d1d4369d718130eda5687ba4f7cff6aabaab
Steve Billings <billings.steve@gmail.com>
2025-03-28 20:02:15 -0400

    test cleaner fix

--- src/corpus/text_cleaner.py
+++ src/corpus/text_cleaner.py
@@ -5,5 +5,5 @@ class TextCleaner:
 
     def clean(self, text_to_clean: str) -> str:
         # Keep word characters, whitespace, and the specified special characters
-        cleaned_text: str = re.sub(r"[^\w\s.,!@#$%&*()+=:;'\"?/<>]", "", text_to_clean)
+        cleaned_text: str = re.sub(r"[^\w\s.,!'-@#$%&*()+=:;'\"?/<>]", "", text_to_clean)
         return cleaned_text
