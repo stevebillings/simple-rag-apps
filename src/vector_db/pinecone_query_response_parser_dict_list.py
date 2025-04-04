2b835fdfa9eab6f61ce3418bb2815c162da28bc3
Steve Billings <billings.steve@gmail.com>
2025-04-04 17:41:11 -0400

    for dict_list: use only the single best match

--- src/vector_db/pinecone_query_response_parser_dict_list.py
+++ src/vector_db/pinecone_query_response_parser_dict_list.py
@@ -7,10 +7,7 @@ class PineconeQueryResponseParserDictList(PineconeQueryResponseParser):
 
     def parse_relevant_content_from_query_response(self, query_response: Dict[str, Any]) -> List[str]:
         results: List[Dict[str, Any]] = query_response["matches"]
-        relevant_content_list: List[str] = []
-        for result in results:
-            score: float = result["score"]
-            if score > 0.5:
-                metadata_str: str = json.dumps(result["metadata"])
-                relevant_content_list.append(metadata_str)
-        return relevant_content_list
+
+        result = results[0]
+        metadata_str: str = json.dumps(result["metadata"])
+        return [metadata_str]
